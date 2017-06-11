668497a77e84962bdcba1fd7e89f7ffa5728b1b9
Mikhail Cheberya <mikhail@idope.ru>
2017-06-11 23:45:46 +0300

    сделал непрерывный рендер кадров

--- javascript/SpaceInvaders.js
+++ javascript/SpaceInvaders.js
@@ -1,9 +1,11 @@
 import { CanvasWidth, CanvasHeight } from './constants/CanvasSizes';
+import requestAnimationFrame from './utils/requestAnimationFrame';
 
 export default class SpaceInvaders {
     constructor( canvas ) {
         this.canvas = canvas;
         this.setCanvasSizes();
+        this.init();
     }
 
     setCanvasSizes() {
@@ -11,4 +13,12 @@ export default class SpaceInvaders {
         this.canvas.height = CanvasHeight;
         this.ctx = this.canvas.getContext('2d');
     }
+
+    init() {
+        requestAnimationFrame( this.renderFrame );
+    }
+
+    renderFrame = () => {
+        requestAnimationFrame( this.renderFrame );
+    };
 }
