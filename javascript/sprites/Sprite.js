4419d91558e414cf5bd91cfa5e5ba19b283a0357
Mikhail Cheberya <mikhail@idope.ru>
2017-06-12 14:55:59 +0300

    доработал абстракт спрайтов

--- javascript/sprites/Sprite.js
+++ javascript/sprites/Sprite.js
@@ -1,11 +1,38 @@
-export class Sprite {
+const SpriteBuffer = {}; // static
+
+export class Sprite { // Abstract
+    constructor( ctx ) {
+        this.ctx    = ctx;
+        this.id     = 'Sprite';
+        this.width  = 0;
+        this.height = 0;
+        this.color  = 'black';
+    }
+
+    afterProps() {
+        if (this.id in SpriteBuffer === false) {
+            this.createBuffer();
+        }
+    }
+
+    createBuffer() {
+        let buffer = document.createElement('canvas');
+            buffer.width = this.width;
+            buffer.height = this.height;
+
+        let bufferCtx = buffer.getContext('2d');
+            bufferCtx.style( this.color );
+
+        this.draw( bufferCtx );
+
+        SpriteBuffer[ this.id ] = [ buffer ];
+    }
+
     draw( ctx ) {}
 
-    drawAt( ctx, x, y ) {
-        ctx.translate( x, y );
-        ctx.style( this.color );
-        this.draw( ctx );
-        ctx.translate( -x, -y );
+    renderAt( x, y ) {
+        const img = SpriteBuffer[ this.id ][0];
+        this.ctx.drawImage( img, x, y );
     }
 }
 
