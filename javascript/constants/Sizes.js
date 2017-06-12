43dff153f6ee4a1ac3d1fcca8b320fb76b98a6b9
Mikhail Cheberya <mikhail@idope.ru>
2017-06-12 15:17:52 +0300

    добавил рамку
    и добавил константы

--- javascript/constants/Sizes.js
+++ javascript/constants/Sizes.js
@@ -1,8 +1,12 @@
 export const CanvasWidth = 1000;
 export const CanvasHeight = CanvasWidth/16*9;
 
+export const FrameWidth = 5;
+export const FieldWidth = CanvasWidth - FrameWidth*2;
+
 export const BorderWidth = 3;
 export const BorderOffset = 60;
+export const BorderTopPosition = Math.round( CanvasHeight - BorderOffset );
 
 export const PlayerOffset = BorderOffset + BorderWidth + 5;
 export const PlayerTopPosition = Math.round( CanvasHeight - PlayerOffset );
--- javascript/elements/Background.js
+++ javascript/elements/Background.js
@@ -1,5 +1,5 @@
 import Element from './Element';
-import { CanvasHeight, BorderWidth, BorderOffset } from '../constants/Sizes';
+import { FrameWidth, FieldWidth, BorderWidth, BorderTopPosition } from '../constants/Sizes';
 import { BackgroundColor, PlayerColor } from '../constants/Colors';
 
 export default class Background extends Element {
@@ -7,6 +7,6 @@ export default class Background extends Element {
         let { width, height } = this.ctx.canvas;
         this.ctx
             .style( BackgroundColor ).fR( 0, 0, width, height )
-            .style( PlayerColor ).fR( 0, Math.round( CanvasHeight - BorderOffset ), width, -BorderWidth );
+            .style( PlayerColor ).fR( FrameWidth, BorderTopPosition, FieldWidth, -BorderWidth );
     }
 }
