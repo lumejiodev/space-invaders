69db2a54d68597d93aa01f5b3c067f9cf95ca4d1
Mikhail Cheberya <mikhail@idope.ru>
2017-06-12 15:27:16 +0300

    отрисовка в зависимости от положения игрока

--- javascript/elements/Player.js
+++ javascript/elements/Player.js
@@ -1,15 +1,26 @@
 import Element from './Element';
 import PlayerSprite from '../sprites/Player';
-import { PlayerTopPosition } from '../constants/Sizes';
+import { FrameWidth, FieldWidth, PlayerTopPosition } from '../constants/Sizes';
 
 export default class Player extends Element {
+    constructor( ...args ) {
+        super( ...args );
+
+        this.pos = 0; // положение от 0 до 1
+        this.maxPos = FieldWidth - this.spriteWidth;
+    }
+
     attachSprite() {
         this.spriteWidth = 52;
         this.spriteHeight = 32;
         this.sprite = new PlayerSprite( this.ctx, this.spriteWidth, this.spriteHeight );
     }
 
+    get position() {
+        return FrameWidth + this.pos * this.maxPos;
+    }
+
     render() {
-        this.sprite.renderAt( 50, PlayerTopPosition - this.spriteHeight );
+        this.sprite.renderAt( this.position, PlayerTopPosition - this.spriteHeight );
     }
 }
