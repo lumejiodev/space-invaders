109cc816f7ec5f3380a35bacf2a3d0d924231443
Mikhail Cheberya <mikhail@idope.ru>
2017-06-14 13:13:18 +0300

    чуть изящнее код

--- javascript/elements/Score.js
+++ javascript/elements/Score.js
@@ -23,15 +23,20 @@ export default class Score extends Element {
         this.totalLives--;
     }
 
+    measureText( text ) {
+        return this.ctx.measureText( text ).width;
+    }
+
     render() {
-        let baseHorzPosition = FrameWidth + ScoreOffset;
-        let scoreHorzPosition = baseHorzPosition + this.ctx.measureText( TextPoints ).width + ScoreOffset/2;
-        let livesTextHorzPosition = scoreHorzPosition + this.ctx.measureText( this.totalScore ).width + ScoreOffset*2;
-        let livesHorzPosition = livesTextHorzPosition + this.ctx.measureText( TextLives ).width + ScoreOffset/2;
+        let baseHorzPosition        = FrameWidth + ScoreOffset;
+        let scoreHorzPosition       = baseHorzPosition + this.measureText( TextPoints ) + ScoreOffset/2;
+        let livesTextHorzPosition   = scoreHorzPosition + this.measureText( this.totalScore ) + ScoreOffset*2;
+        let livesHorzPosition       = livesTextHorzPosition + this.measureText( TextLives ) + ScoreOffset/2;
+
         this.ctx
-            .style( ScoreText ).text( TextPoints, baseHorzPosition, this.vertPosition )
+            .style( ScoreText   ).text( TextPoints, baseHorzPosition, this.vertPosition )
             .style( ScorePoints ).text( this.totalScore, scoreHorzPosition, this.vertPosition )
-            .style( ScoreText ).text( TextLives, livesTextHorzPosition, this.vertPosition )
-            .style( ScoreLives ).text( this.totalLives, livesHorzPosition, this.vertPosition );
+            .style( ScoreText   ).text( TextLives, livesTextHorzPosition, this.vertPosition )
+            .style( ScoreLives  ).text( this.totalLives, livesHorzPosition, this.vertPosition );
     }
 }
